08d7f2b272bf1d5f97b4cb6395b6675359cd7f12
edray28 <edraytuyor@gmail.com>
2021-04-16 14:09:42 +0800

    Second Commit Added Elements

--- WordtoNumber.py
+++ WordtoNumber.py
@@ -1,8 +1,11 @@
-# WordtoNumber Calculator v0.1
-import tkinter 
+# WordtoNumber Calculator v0.2
+import tkinter as TK 
+import math
 
 #The Main Gui
-class myGui(tkinter.Tk):
+def helloCallBack():
+    print('TESTING')
+class my_gui(TK.Tk):
     def __init__(self,w=500,h=500): 
         super().__init__() 
         self.title("Word2Number Calculator")
@@ -13,7 +16,23 @@ class myGui(tkinter.Tk):
         x = (ws/2) - (w/2)    
         y = (hs/2) - (h/2)
         self.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        self.gui_elements()
+        #Elements
+    def gui_elements(self):
+        lbl1 = TK.Label(self,text= "Enter a Number in Word Format(ex: One Plus Two)", font="Helvetica")
+        txt_field = TK.Text(self, font="Helvetica")
+        calc_btn = TK.Button(self, text="Calculate", bd=5, font="Helvetica", command= helloCallBack)
+        lbl1.pack()
+        txt_field.pack()
+        calc_btn.pack()
 
+        
 if __name__ == "__main__":
-    main_app = myGui()
+    main_app = my_gui()
     main_app.mainloop()
+    
+    
+
+
+
+    
